de912533af0473b76783a586a93a489b368c6e30
Muhammad Ali <malibutt074@gmail.com>
2024-12-20 17:44:32 +0500

    all

---
+++ class 14/script.js
@@ -0,0 +1,16 @@
+// for(let a = 1; a<=10; a++ ) 
+// {
+//     console.log("pakistan Zindabaad");
+// }
+
+function str(){
+    console.log("Pakistan")
+}
+
+ str();
+
+ //ali
+
+ console.log("ali");
+
+ 
