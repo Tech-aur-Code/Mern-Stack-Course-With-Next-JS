c2cfb52c82dd3c8060451df961b21f7c99b03e0e
Muhammad Ali <153981195+ProfessionalMentor@users.noreply.github.com>
2024-12-21 23:08:25 +0500

    poject 1

---
+++ class 16/script.js
@@ -0,0 +1,11 @@
+
+// Dom Manuplation 
+
+document.getElementById("12").style.color = 'red';
+document.getElementById("13").style.color = "rgb(248, 19, 240)";
+document.getElementById("14").style.color = 'blue';
+document.getElementById("15").style.color = ' cornflowerblue';
+
+
+ 
+
