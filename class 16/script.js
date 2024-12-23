6789604d60e7dad779ea3323f4fd997e9ed4f11e
Muhammad Ali <153981195+ProfessionalMentor@users.noreply.github.com>
2024-12-24 00:37:29 +0500

    contact us

--- class 16/script.js
+++ class 16/script.js
@@ -6,6 +6,11 @@ document.getElementById("13").style.color = "rgb(248, 19, 240)";
 document.getElementById("14").style.color = 'blue';
 document.getElementById("15").style.color = ' cornflowerblue';
 
+document.getElementById("16").style.color = "red";
+document.getElementById("17").style.color = ' rgb(9, 104, 237)';
+document.getElementById("18").style.color = ' red';
+
+
 
  
 
