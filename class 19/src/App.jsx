a01d5c5ef01b7c5f9a8cae181f69246a83cd2c76
Muhammad Ali <153981195+ProfessionalMentor@users.noreply.github.com>
2025-01-01 21:10:06 +0500

    all

---
+++ class 19/src/App.jsx
@@ -0,0 +1,19 @@
+import 'react'
+import Button from './components/button'
+
+function App() {
+  return (
+    <>
+    
+      <h1 className=''>
+        Hello World
+      </h1>
+      <p>This is React Second App</p>
+    <Button/>
+    
+    
+    </>
+  )
+}
+
+export default App
---
+++ class 19/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import 'react'
+import "./Button.css"
+function Button() {
+  return (
+    <div>
+        <button>Click Here</button>
+    </div>
+  )
+}
+
+export default Button
