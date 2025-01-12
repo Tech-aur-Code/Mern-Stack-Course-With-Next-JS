31a07f5df78640c953da001a59fa72e259b7bfa8
Muhammad Ali <153981195+ProfessionalMentor@users.noreply.github.com>
2025-01-12 22:34:58 +0500

    a

--- class 24/src/App.jsx
+++ class 24/src/App.jsx
@@ -2,7 +2,15 @@ import  'react'
 
 function App() {
   return (
-    <div>App</div>
+   <>
+   <div>
+    <h1 className='text-xl font-sans' >hello</h1>
+    <p className='bg-slate-950 text-white' >This is the Class no 23</p>
+    <input type="text" className='h-14 w-72 font-semibold m-8 visible 
+    border-2 outline-none border-black rounded-md' />
+    
+   </div>
+   </>
   )
 }
 
